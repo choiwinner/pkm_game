abf09f624980ab4d78dbd22ded62ecf245fb8bc2
choiwinner <babyrain@naver.com>
2024-10-02 03:01:25 +0900

    ;feet 스테이지 에러 수정 완료

--- pkmon_game.py
+++ pkmon_game.py
@@ -87,12 +87,12 @@ def quiz():
             # Clear all those elements:
             placeholder.empty()
 
-def quiz_one(index,quiz_list,answer): 
+def quiz_one1(index_n,quiz_list,answer): 
 
     #with placeholder.container():
-    st.write(f"{index}번째 문제입니다.")
+    st.write(f"{index_n}번째 문제입니다.")
     #status = st.radio('정답을 맞추세요', quiz_list)
-    #st.write(answer)
+    st.write(answer)
     #if status == answer:
     #    st.info('정답입니다.')
     #    st.session_state["stage"] = st.session_state["stage"]+1
@@ -104,19 +104,17 @@ def quiz_one(index,quiz_list,answer):
     #answer_n = None
     #answer_tmp = None
 
+    #st.session_state["answer_n"] = 0
+    #st.session_state["answer"] = ''
+
+    #st.write(st.session_state["answer_n"])
+
     for index,i in enumerate(quiz_list):
         st.write(f'{index+1}.{i}')
 
-    try:
-        answer_n = int(st.text_input('정답 번호를 입력하세요.'))
-        answer_tmp = quiz_list[answer_n-1]
-    except:
-        answer_n = None
-        answer_tmp = None
-        st.write('1~4까지 숫자만 입력하세요.')
-    #status = st.radio('정답을 맞추세요', list_f)
-    else:
-        if answer_tmp == answer:
+    if val1 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val1-1] == answer:
             st.info('정답입니다.')
             st.session_state["stage"] = st.session_state["stage"]+1
             st.session_state["health"] = st.session_state["health"]+10
@@ -125,7 +123,198 @@ def quiz_one(index,quiz_list,answer):
             st.session_state["stage"] = st.session_state["stage"]+1
             st.session_state["health"] = st.session_state["health"]-10
 
-    time.sleep(3)
+def quiz_one2(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val2 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val2-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one3(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val3 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val3-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one4(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val4 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val4-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one5(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val5 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val5-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one6(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val6 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val6-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one7(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val7 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val7-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one8(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val8 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val8-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+def quiz_one9(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val9 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val9-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+
+def quiz_one10(index_n,quiz_list,answer): 
+
+    st.write(f"{index_n}번째 문제입니다.")
+    st.write(answer)
+
+    for index,i in enumerate(quiz_list):
+        st.write(f'{index+1}.{i}')
+
+    if val10 := int(st.number_input('정답 번호를 입력하세요.')):
+
+        if quiz_list[val10-1] == answer:
+            st.info('정답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]+10
+        else:
+            st.warning('오답입니다.')
+            st.session_state["stage"] = st.session_state["stage"]+1
+            st.session_state["health"] = st.session_state["health"]-10
+
+    #try:
+    #    
+    #    answer_tmp = quiz_list[st.session_state["answer"]-1]
+    #except:
+    #    st.session_state["answer"] = 0
+    #    answer_tmp = None
+    #    st.write('1~4까지 숫자만 입력하세요.')
+    ##status = st.radio('정답을 맞추세요', list_f)
+#
+    #else:
+    #    if answer_tmp == answer:
+    #        st.write('최재일3')
+    #        st.info('정답입니다.')
+    #        st.session_state["stage"] = st.session_state["stage"]+1
+    #        st.session_state["health"] = st.session_state["health"]+10
+    #    else:
+    #        st.write('최재일4')
+    #        st.warning('오답입니다.')
+    #        st.session_state["stage"] = st.session_state["stage"]+1
+    #        st.session_state["health"] = st.session_state["health"]-10
+
+    #time.sleep(10)
     #placeholder.empty()
 
 
@@ -149,13 +338,17 @@ if __name__=='__main__':
     )
 
     if "health" not in st.session_state:
-        st.session_state["health"] = 100
+        st.session_state["health"] = 0
     if "name" not in st.session_state:
         st.session_state["name"] = '아무개'
     if "stage" not in st.session_state:
         st.session_state["stage"] = 1
     if "start" not in st.session_state:
         st.session_state["start"] = 0
+    if "answer_n" not in st.session_state:
+        st.session_state["answer_n"] = 0
+    if "answer" not in st.session_state:
+        st.session_state["answer"] = ''
 
     horizontal_bar = "<hr style='margin-top: 0; margin-bottom: 0; height: 1px; border: 1px solid #635985;'><br>"    # thin divider line
 
@@ -176,7 +369,7 @@ if __name__=='__main__':
             st.session_state.clear()
             st.cache_data.clear()
             st.cache_resource.clear()
-            st.session_state["health"] = 100
+            st.session_state["health"] = 0
             st.session_state["name"] = '아무개'
             st.session_state["stage"] = 1
             st.session_state["start"] = 0
@@ -206,12 +399,7 @@ if __name__=='__main__':
         wrong_list2 =[]
         wrong_list2 = list(map(lambda x: df["Name"][x-1],wrong_list))
 
-        st.write(answer_list2)
-
         list_f = random_make3(answer_list2,wrong_list2)
-
-        st.write(answer_list2[0])
-        st.write(list_f[0])
 
         #st.write(list_f)
         
@@ -224,43 +412,43 @@ if __name__=='__main__':
                 if st.session_state['stage'] == 1:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[0],answer_list2[0])
+                        quiz_one1(st.session_state["stage"],list_f[0],answer_list2[0])
                 elif st.session_state['stage'] == 2:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[1],answer_list2[1])
+                        quiz_one2(st.session_state["stage"],list_f[1],answer_list2[1])
                 elif st.session_state['stage'] == 3:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[2],answer_list2[2])
+                        quiz_one3(st.session_state["stage"],list_f[2],answer_list2[2])
                 elif st.session_state['stage'] == 4:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[3],answer_list2[3])        
+                        quiz_one4(st.session_state["stage"],list_f[3],answer_list2[3])        
                 elif st.session_state['stage'] == 5:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[4],answer_list2[4])
+                        quiz_one5(st.session_state["stage"],list_f[4],answer_list2[4])
                 elif st.session_state['stage'] == 6:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[5],answer_list2[5])
+                        quiz_one6(st.session_state["stage"],list_f[5],answer_list2[5])
                 elif st.session_state['stage'] == 7:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[6],answer_list2[6])
+                        quiz_one7(st.session_state["stage"],list_f[6],answer_list2[6])
                 elif st.session_state['stage'] == 8:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[7],answer_list2[7])
+                        quiz_one8(st.session_state["stage"],list_f[7],answer_list2[7])
                 elif st.session_state['stage'] == 9:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[8],answer_list2[8])
+                        quiz_one9(st.session_state["stage"],list_f[8],answer_list2[8])
                 elif st.session_state['stage'] == 10:
                     placeholder.empty()
                     with placeholder.container():
-                        quiz_one(st.session_state["stage"],list_f[9],answer_list2[9])
+                        quiz_one10(st.session_state["stage"],list_f[9],answer_list2[9])
                 else:
                     placeholder.empty()
                     with placeholder.container():
@@ -268,6 +456,7 @@ if __name__=='__main__':
                         user = st.session_state["name"]
                         st.write(f'축하합니다. {user}님')
                         st.write(f'최종 점수는 {final_score}점 입니다.')
+                        st.stop()
                     
 
         #if st.session_state.stage ==1:
